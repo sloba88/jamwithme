a86efb0dce2dd486109781477f31441e08d2cbe3
Slobodan <slobodan.stanic88@gmail.com>
2013-12-15 01:15:28 +0200

    bootstrap bundle placed and layout in progress

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Jam\CoreBundle\JamCoreBundle(),
             new Jam\WebBundle\JamWebBundle(),
             new Jam\UserBundle\JamUserBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
