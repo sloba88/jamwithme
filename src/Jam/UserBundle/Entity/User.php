bb88b5d1b6a6829f364fe333cc8583b18880450b
Slobodan <slobodan.stanic88@gmail.com>
2014-05-04 19:31:13 +0300

    array collection included

--- src/Jam/UserBundle/Entity/User.php
+++ src/Jam/UserBundle/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace Jam\UserBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
