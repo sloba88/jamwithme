f6ac2e0ea76b07477bc579e7a84544dfccc6f76b
Slobodan Stanic <stanic.slobodan88@gmail.com>
2014-06-18 01:47:34 +0300

    form for messages

--- src/Jam/MessageBundle/Controller/DefaultController.php
+++ src/Jam/MessageBundle/Controller/DefaultController.php
@@ -11,41 +11,56 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/message/send/{username}")
+     * @Route("/message/send/{username}", name="send_message")
      * @Template()
      */
     public function sendAction($username)
     {
         $userManager = $this->get('fos_user.user_manager');
         $user = $userManager->findUserByUsername($username);
-
         $me = $this->container->get('security.context')->getToken()->getUser();
-
-        $repository = $this->get('doctrine_mongodb')
-            ->getManager()
-            ->getRepository('JamMessageBundle:Inbox');
-
-        $myInbox = $repository->findOneByUser($me->getId());
-
-        if ($myInbox){
-
-        }else{
-            $myInbox = new Inbox();
-            $myInbox->setUser($me->getId());
-        }
+        $request = $this->get('request_stack')->getCurrentRequest();
 
         $message = new Message();
-        $message->setFrom($me->getId());
-        $message->setTo($user->getId());
-        $message->setMessage("Second message in your inbox!!!");
 
-        $myInbox->addMessage($message);
+        $form = $this->createFormBuilder($message)
+            ->add('message', 'textarea')
+            ->add('send', 'submit')
+            ->getForm();
 
-        $dm = $this->get('doctrine_mongodb')->getManager();
-        $dm->persist($myInbox);
-        $dm->flush();
+        $form->handleRequest($request);
 
-        return array('name' => $username);
+        if ($form->isValid()) {
+
+            $data = $form->getData();
+
+            $repository = $this->get('doctrine_mongodb')
+                ->getManager()
+                ->getRepository('JamMessageBundle:Inbox');
+
+            $myInbox = $repository->findOneByUser($me->getId());
+
+            if ($myInbox){
+
+            }else{
+                $myInbox = new Inbox();
+                $myInbox->setUser($me->getId());
+            }
+
+            $message->setFrom($me->getId());
+            $message->setTo($user->getId());
+            $message->setMessage($data->getMessage());
+
+            $myInbox->addMessage($message);
+
+            $dm = $this->get('doctrine_mongodb')->getManager();
+            $dm->persist($myInbox);
+            $dm->flush();
+
+            return $this->redirect($this->generateUrl('send_message', array('username' => $user->getUsername())));
+        }
+
+        return array('form' => $form->createView());
     }
 
     /**
