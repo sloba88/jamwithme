f302d093306f8977e65c95846b2ee62debecf0b9
Slobodan Stanic <stanic.slobodan88@gmail.com>
2014-07-09 00:38:26 +0300

    shouts added

---
+++ src/Jam/CoreBundle/Entity/Shout.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Jam\CoreBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Jam
+ *
+ * @ORM\Table(name="shouts")
+ * @ORM\Entity
+ */
+class Shout
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\ManyToOne(targetEntity="Jam\UserBundle\Entity\User", inversedBy="jamsCreator")
+     * @ORM\JoinColumn(name="creator_id", referencedColumnName="id", nullable=true)
+     */
+    private $creator;
+
+    /**
+     * @ORM\Column(type="text", nullable=false)
+     *
+     */
+    protected $text;
+
+    /**
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    protected $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set creator
+     *
+     * @param \Jam\UserBundle\Entity\User $creator
+     * @return Shout
+     */
+    public function setCreator(\Jam\UserBundle\Entity\User $creator = null)
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get creator
+     *
+     * @return \Jam\UserBundle\Entity\User 
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Shout
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Shout
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Shout
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+}
--- src/Jam/WebBundle/Controller/HomeController.php
+++ src/Jam/WebBundle/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace Jam\WebBundle\Controller;
 
+use Jam\CoreBundle\Entity\Shout;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
 {
@@ -12,8 +14,35 @@ class HomeController extends Controller
      * @Route("/", name="home")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return array();
+        $shout = new Shout();
+
+        $form = $this->createFormBuilder($shout)
+            ->add('text', 'textarea')
+            ->add('submit', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $me = $this->container->get('security.context')->getToken()->getUser();
+
+            if (!$me){
+                throw $this->createNotFoundException('You shall not pass');
+            }
+
+            $shout->setCreator($me);
+            $em->persist($shout);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->set('success', 'You have shouted successfully.');
+
+            return $this->redirect($this->generateUrl('home'));
+        }
+
+        return array('form' => $form->createView());
     }
 }
