1fc6e9efcb6880a3165c52a41c29b2008caabed3
Slobodan Stanic <stanic.slobodan88@gmail.com>
2015-02-09 23:03:15 +0200

    map fix, style changes

--- src/Jam/LocationBundle/Resources/public/location.js
+++ src/Jam/LocationBundle/Resources/public/location.js
@@ -4,47 +4,54 @@ $(function() {
         var widget = $(this),
             widget_location = widget.find('[id$="_location"]')
 
-        var addresspickerMap = widget_location.find('[id$="_location_address"]').addresspicker({
-            regionBias: 'en',
-            reverseGeocode: true,
-            elements: {
-                map: widget.find('[id$="_location_map"]'),
-                locality: widget.find('[id$="_location_locality"]'),
-                neighborhood: widget.find('[id$="_location_neighborhood"]'),
-                administrative_area_level_3: widget.find('[id$="_administrative_area_level_3"]'),
-                country: widget.find('[id$="_location_country"]'),
-                route: widget.find('[id$="_location_route"]'),
-                postal_code: widget.find('[id$="_location_zip"]'),
-                lat: widget.find('[id$="_location_lat"]'),
-                lng: widget.find('[id$="_location_lng"]')
-            },
-            mapOptions: {
-                center: new google.maps.LatLng(60.1605645, 24.8696196),
-                zoom: 12
-            },
-            updateCallback: function() {
-                showMap(false);
-            }
-        })
 
-        var gmarker = addresspickerMap.addresspicker("marker");
-        gmarker.setVisible(widget_location.attr('data-marker') ? true : false);
-        widget.find('.location_map').on('click', function() {
-            if (!gmarker.getVisible()) {
-                gmarker.setVisible(true);
-                addresspickerMap.addresspicker("updatePosition");
-            }
-        });
-
-        $('[id^="ad_location_address"]').on('change', function() {
-            if (!$(this).val()) {
-                $('[id^="ad_location"]').val('');
-                gmarker.setVisible(false);
-            }
-        });
 
         var showMap = function(boolean) {
-            widget.find('.dropdown-menu').css({'visibility': boolean ? 'visible' : 'hidden'});
+            widget.find('.dropdown-menu').css({'display': boolean ? 'block' : 'none'});
+            if (boolean){
+                var addresspickerMap = widget_location.find('[id$="_location_address"]').addresspicker({
+                    regionBias: 'en',
+                    reverseGeocode: true,
+                    elements: {
+                        map: widget.find('[id$="_location_map"]'),
+                        locality: widget.find('[id$="_location_locality"]'),
+                        neighborhood: widget.find('[id$="_location_neighborhood"]'),
+                        administrative_area_level_3: widget.find('[id$="_administrative_area_level_3"]'),
+                        country: widget.find('[id$="_location_country"]'),
+                        route: widget.find('[id$="_location_route"]'),
+                        postal_code: widget.find('[id$="_location_zip"]'),
+                        lat: widget.find('[id$="_location_lat"]'),
+                        lng: widget.find('[id$="_location_lng"]')
+                    },
+                    mapOptions: {
+                        center: new google.maps.LatLng(60.1605645, 24.8696196),
+                        zoom: 12
+                    },
+                    updateCallback: function() {
+                        //showMap(false);
+                    }
+                });
+
+                var gmarker = addresspickerMap.addresspicker("marker");
+
+                gmarker.setVisible(true);
+                addresspickerMap.addresspicker("updatePosition");
+
+                //gmarker.setVisible(widget_location.attr('data-marker') ? true : false);
+                widget.find('.location_map').on('click', function() {
+                    if (!gmarker.getVisible()) {
+                        gmarker.setVisible(true);
+                        addresspickerMap.addresspicker("updatePosition");
+                    }
+                });
+
+                $('[id^="ad_location_address"]').on('change', function() {
+                    if (!$(this).val()) {
+                        $('[id^="ad_location"]').val('');
+                        gmarker.setVisible(false);
+                    }
+                });
+            }
         }
 
         widget.find('*').on('focus', function(e) {
