7a34220e56b05b92257ff3152750471661c40656
sloba88 <stanic.slobodan88@gmail.com>
2015-07-04 21:23:14 +0000

    event for profile edit to remove location if empty

--- src/Jam/UserBundle/Entity/User.php
+++ src/Jam/UserBundle/Entity/User.php
@@ -5,7 +5,6 @@ namespace Jam\UserBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Jam\CoreBundle\Model\Image;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
