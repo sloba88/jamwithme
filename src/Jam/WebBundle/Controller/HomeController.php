eb084cbf8791ca4a356106240d1a98f9f4230b4a
sloba88 <stanic.slobodan88@gmail.com>
2015-11-27 11:45:41 +0000

    removed unnessesary function

--- src/Jam/WebBundle/Controller/HomeController.php
+++ src/Jam/WebBundle/Controller/HomeController.php
@@ -36,22 +36,4 @@ class HomeController extends Controller
             'form' => $form->createView()
         );
     }
-
-    public function unserialize_php($session_data) {
-        $return_data = array();
-        $offset = 0;
-        while ($offset < strlen($session_data)) {
-            if (!strstr(substr($session_data, $offset), "|")) {
-                throw new Exception("invalid data, remaining: " . substr($session_data, $offset));
-            }
-            $pos = strpos($session_data, "|", $offset);
-            $num = $pos - $offset;
-            $varname = substr($session_data, $offset, $num);
-            $offset += $num + 1;
-            $data = unserialize(substr($session_data, $offset));
-            $return_data[$varname] = $data;
-            $offset += strlen(serialize($data));
-        }
-        return $return_data;
-    }
 }
