49395b47e6f336ac4255b30cab48658fbd5ff311
sloba88 <stanic.slobodan88@gmail.com>
2015-12-15 13:59:32 +0000

    fixed settings tab height

--- web/assets/js/settings.js
+++ web/assets/js/settings.js
@@ -257,7 +257,7 @@ function renameCollectionNames($selection) {
 
 function setTabsHeight() {
     if ($(window).width() > 992) {
-        $('.page-settings .view-tab.with-scrollbar').height($(window).height() - 300);
+        $('.page-settings .view-tab.with-scrollbar').height($(window).height() - 250);
     }
     return true;
 }
