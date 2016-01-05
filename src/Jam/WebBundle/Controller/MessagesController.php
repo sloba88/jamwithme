5340781031bb459c394de76cc7134d0c038e6ad5
sloba88 <stanic.slobodan88@gmail.com>
2016-01-05 10:19:59 +0000

    cleaned up

--- src/Jam/WebBundle/Controller/MessagesController.php
+++ src/Jam/WebBundle/Controller/MessagesController.php
@@ -5,8 +5,6 @@ namespace Jam\WebBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Jam\CoreBundle\Entity\Subscription;
-use Symfony\Component\HttpFoundation\Request;
 
 class MessagesController extends Controller
 {
