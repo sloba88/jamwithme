f43ad55e79c0af6205c0fec396ad9d88bd29b93d
sloba88 <stanic.slobodan88@gmail.com>
2016-01-22 15:21:02 +0000

    register page is the new home page, base layout unified

--- web/landing/js/script.js
+++ web/landing/js/script.js
@@ -1,106 +1,93 @@
+'use strict';
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+        results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function parallax() {
+    var $window = $(window);
+
+    $('[data-type]').each(function() {
+        $(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
+        $(this).data('Xposition', $(this).attr('data-Xposition'));
+        $(this).data('speed', $(this).attr('data-speed'));
+    });
+
+    $('section[data-type="background"]').each(function(){
+
+        var $self = $(this),
+            offsetCoords = $self.offset(),
+            topOffset = offsetCoords.top;
+
+        if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
+            ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+
+            var yPos = -( ($window.scrollTop() - $self.offset().top) / $self.data('speed'));
+
+            if ($self.data('offsetY')) {
+                yPos += $self.data('offsetY');
+            }
+
+            var coords = '50% '+ yPos + 'px';
+
+            $self.css({ backgroundPosition: coords });
+
+        } // in view
+
+        $(window).scroll(function() {
+
+            if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
+                ( (topOffset + $self.height()) > $window.scrollTop() ) ) {
+
+                var yPos = -( ($window.scrollTop() - $self.offset().top) / $self.data('speed'));
+
+                if ($self.data('offsetY')) {
+                    yPos += $self.data('offsetY');
+                }
+
+                var coords = '50% '+ yPos + 'px';
+
+                $self.css({ backgroundPosition: coords });
+
+            } // in view
+
+        }); // window scroll
+
+    });	// each data-type
+}
+
 $(document).ready(function(){
 
 	parallax();
 
 	$('#horizontalmenu').ddscrollSpy();
 
-    $(".submitBtn").on('click', function(e){
+    $('input[type=checkbox]').next('label').prepend('<span></span>');
+    $('.info-popover').popover();
 
-        var $myForm = $(".submitBtn").parents('form');
-        if (!$myForm[0].checkValidity()) {
-
-        }else{
-            e.preventDefault();
-            $.ajax({
-                data: {'email': $(".inputField").val()},
-                type: 'POST',
-                url: 'http://jamifind.com/app_dev.php/subscription/add',
-                success: function(result){
-                    console.log(result);
-                    if (result.status == 'success'){
-                        $(".subscribeBox >").fadeOut('slow', function(){
-                            $(".subscribeBox").html('<h4>Thank you for your subscription.</h4>');
-                        });
-                    }else if(result.status == 'error'){
-                        $('.formError').remove();
-                        $(".subscribeBox").append('<p class="formError">'+result.message+'</p>');
-                    }
-                }
-            });
-        }
+    $('#fos_user_registration_form_plainPassword_second').bind('cut copy paste',function(e) {
+        e.preventDefault();
     });
 
+    $('#fos_user_registration_form_username').on('keyup', function(){
+        var start = this.selectionStart,
+            end = this.selectionEnd,
+            str = $(this).val();
+        str = str.replace(/[^A-Za-z0-9 ]/g, '');
+        $(this).val(str);
 
+        this.setSelectionRange(start, end);
+    });
 
+    if (getParameterByName('email')) {
+        $('#fos_user_registration_form_email').val(getParameterByName('email'));
+        $('#fos_user_resetting_request_username').val(getParameterByName('email'));
+    }
 }); // document ready
 
-function parallax() {
-	$window = $(window);
-	
-	$('[data-type]').each(function() {	
-		$(this).data('offsetY', parseInt($(this).attr('data-offsetY')));
-		$(this).data('Xposition', $(this).attr('data-Xposition'));
-		$(this).data('speed', $(this).attr('data-speed'));
-	});
-	
-	$('section[data-type="background"]').each(function(){
 
-		var $self = $(this),
-		offsetCoords = $self.offset(),
-		topOffset = offsetCoords.top;
-
-		if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
-			( (topOffset + $self.height()) > $window.scrollTop() ) ) {
-
-			var yPos = -( ($window.scrollTop() - $self.offset().top) / $self.data('speed'));  
-
-		if ($self.data('offsetY')) {
-			yPos += $self.data('offsetY');
-		}
-
-		var coords = '50% '+ yPos + 'px';
-
-		$self.css({ backgroundPosition: coords }); 
-
-			}; // in view
-
-			$(window).scroll(function() {
-
-				if ( ($window.scrollTop() + $window.height()) > (topOffset) &&
-					( (topOffset + $self.height()) > $window.scrollTop() ) ) {
-
-					var yPos = -( ($window.scrollTop() - $self.offset().top) / $self.data('speed'));  
-				
-				if ($self.data('offsetY')) {
-					yPos += $self.data('offsetY');
-				}
-				
-				var coords = '50% '+ yPos + 'px';
-
-				$self.css({ backgroundPosition: coords }); 
-
-			}; // in view			
-
-		}); // window scroll
-			
-	});	// each data-type
-}
-
-function backToTop() {
-	var offset = 220;
-	var duration = 500;
-
-	if ($(this).scrollTop() > offset) {
-		$('.back-to-top').fadeIn(duration);
-	} else {
-		$('.back-to-top').fadeOut(duration);
-	}
-
-	$('.back-to-top').click(function(event) {
-		event.preventDefault();
-		$('html, body').animate({scrollTop: 0}, duration);
-		return false;
-	});
-}
 
 
