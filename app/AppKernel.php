19428149a5f8d650c92037ec270e94844d036a31
sloba88 <stanic.slobodan88@gmail.com>
2016-01-27 09:56:10 +0000

    moved excel bundle to prod

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,13 +36,13 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new SunCat\MobileDetectBundle\MobileDetectBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Liuggio\ExcelBundle\LiuggioExcelBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Liuggio\ExcelBundle\LiuggioExcelBundle();
         }
 
         return $bundles;
