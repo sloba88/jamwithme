f4dcfb83ff1e0d4acb9d603411ff9cadad775c20
sloba88 <stanic.slobodan88@gmail.com>
2016-02-08 12:03:58 +0000

    trying full screen on mobile

--- web/assets/js/messaging.js
+++ web/assets/js/messaging.js
@@ -110,6 +110,21 @@ function sendMessage(self) {
     self.val('');
 }
 
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    }
+    else {
+        cancelFullScreen.call(doc);
+    }
+}
+
 $(function() {
 
     //only on messages page
@@ -251,10 +266,13 @@ $(function() {
         openedConversation.userId = '';
 
         if (isMobile) {
+            window.scrollTo(0, 1);
+
             if ($(window).height() <= 568){
                 $('body').css({ 'overflow': 'auto' });
                 $('body .container-fluid').css({ 'overflow-x': 'auto' });
                 $('body .container-fluid').css({ 'height': 'auto' });
+                toggleFullScreen();
             }
         }
 
