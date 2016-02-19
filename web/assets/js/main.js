07bc514e02a4b0dfaa79ae70f2ce5b5c05a4a6cf
sloba88 <stanic.slobodan88@gmail.com>
2016-02-19 09:15:08 +0000

    added invite friends for dev

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -399,6 +399,27 @@ function getUserShouts() {
     });
 }
 
+function auth() {
+    var config = {
+        'client_id': '429745829616-l08gihug3r9o9fvj76oh0jdts3sq4g6j.apps.googleusercontent.com',
+        'scope': 'https://www.google.com/m8/feeds'
+    };
+    gapi.auth.authorize(config, function() {
+        fetch(gapi.auth.getToken());
+
+    });
+}
+
+function fetch(token) {
+    $.ajax({
+        url: 'https://www.google.com/m8/feeds/contacts/default/full?access_token=' + token.access_token + '&alt=json',
+        dataType: 'jsonp',
+        success:function(data) {
+            console.log(JSON.stringify(data));
+        }
+    });
+}
+
 $(function() {
 
     //checks if touch device
@@ -632,6 +653,11 @@ $(function() {
                 }
             }
         });
+    });
+
+    $('.sidebar-nav').on('click', '.invite-friends', function(e){
+        e.preventDefault();
+        auth();
     });
 
     //scrollbar plugin
