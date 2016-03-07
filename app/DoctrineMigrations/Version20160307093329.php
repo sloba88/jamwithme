9185a866c909d8a788fffb6783450b83693efde3
sloba88 <stanic.slobodan88@gmail.com>
2016-03-07 08:40:58 +0000

    genres replaced and put to migrations

--- app/DoctrineMigrations/Version20160307093329.php
+++ app/DoctrineMigrations/Version20160307093329.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace Jam\CoreBundle\DataFixtures\ORM;
+namespace Application\Migrations;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
 use Jam\CoreBundle\Entity\Genre;
 use Jam\CoreBundle\Entity\GenreCategory;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadGenresData implements FixtureInterface, ContainerAwareInterface
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160307093329 extends AbstractMigration implements ContainerAwareInterface
 {
-    /**
-     * @var ContainerInterface
-     */
     private $container;
 
     public function setContainer(ContainerInterface $container = null)
@@ -22,12 +22,19 @@ class LoadGenresData implements FixtureInterface, ContainerAwareInterface
     }
 
     /**
-     * {@inheritDoc}
+     * @param Schema $schema
      */
-    public function load(ObjectManager $manager)
+    public function up(Schema $schema)
     {
+        $this->addSql("SET FOREIGN_KEY_CHECKS=0;");
+        $this->addSql("TRUNCATE TABLE genres");
+        $this->addSql("TRUNCATE TABLE genre_categories");
+    }
 
+    public function postUp(Schema $schema)
+    {
         $phpExcelObject = $this->container->get('phpexcel')->createPHPExcelObject($this->container->get('kernel')->getRootDir() . '/../data/genres.xls');
+        $manager = $this->container->get('doctrine.orm.entity_manager');
 
         //  Get worksheet dimensions
         $sheet = $phpExcelObject->getSheet(0);
@@ -50,10 +57,10 @@ class LoadGenresData implements FixtureInterface, ContainerAwareInterface
                     }
 
                     $genre = new Genre();
+                    print($cell->getValue());
                     $genre->setName($cell->getValue());
                     $genre->setCategory($genreCategory);
                     $manager->persist($genre);
-
                 }
                 if ($genreCategory->getname()) {
                     $manager->persist($genreCategory);
@@ -62,5 +69,16 @@ class LoadGenresData implements FixtureInterface, ContainerAwareInterface
         }
 
         $manager->flush();
+
+        $this->addSql("SET FOREIGN_KEY_CHECKS=1;");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
     }
 }
