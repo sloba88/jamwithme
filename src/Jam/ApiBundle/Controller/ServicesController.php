a71e8a8ef76aa340650a9734961d7b26b42bea88
sloba88 <stanic.slobodan88@gmail.com>
2016-12-17 18:41:58 +0000

    services backend done

---
+++ src/Jam/ApiBundle/Controller/ServicesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Jam\ApiBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class ServicesController extends FOSRestController
+{
+    /**
+     * @Get("/services/find", name="services_find")
+     */
+    public function findAction()
+    {
+        $request = $this->get('request_stack')->getCurrentRequest();
+        $me = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        if (!$me->getLocation()) {
+            $view = $this->view(array(
+                'message'    => 'No location set.',
+                'success'    => false,
+                'data' => array()
+            ), 200);
+
+            return $this->handleView($view);
+        }
+
+        $request->getSession()->save();
+        $services = $this->get('search.services')->getElasticSearchResult($request->query->get('distance'));
+
+        $servicesData = array();
+
+        foreach($services AS $k=> $s){
+
+            /* @var $s \Jam\CoreBundle\Entity\Service */
+
+            $data_array = array(
+                'displayName' => $s->getDisplayName(),
+                'lat' => $s->getLat() ? $s->getLat() : '',
+                'lng' => $s->getLon() ? $s->getLon() : ''
+            );
+            array_push($servicesData, $data_array);
+        }
+
+        $view = $this->view(array(
+            'status'    => 'success',
+            'data' => $servicesData,
+        ), 200);
+
+        return $this->handleView($view);
+    }
+}
