f20bd69b74f06b8ac1fe65497a64667b956cde01
sloba88 <stanic.slobodan88@gmail.com>
2017-02-10 21:25:47 +0000

    new admin tools

--- src/Jam/WebBundle/Controller/Admin/UsersController.php
+++ src/Jam/WebBundle/Controller/Admin/UsersController.php
@@ -22,11 +22,24 @@ class UsersController extends Controller
      * @Route("/users-list", name="admin_users_list")
      * @Template
      */
-    public function listAction()
+    public function listAction(Request $request)
     {
+        $onlyTeachers = $request->query->get('onlyTeachers') ? 1 : 0;
+        $location = $request->query->get('location');
+
         $musicians = $this->getDoctrine()
             ->getRepository('JamUserBundle:User')
-            ->findAll();
+            ->createQueryBuilder('m')
+            ->where('m.isTeacher = ' .$onlyTeachers);
+
+        if ($location) {
+            $musicians = $musicians->leftJoin('m.location', 'l')
+                ->andWhere('l.administrative_area_level_3 = ?1')
+                ->setParameter(1, $location);
+        }
+
+        $musicians = $musicians->getQuery()
+            ->getResult();
 
         $repository = $this->getDoctrine()
             ->getRepository('JamUserBundle:User');
